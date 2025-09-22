5da5bddcb3fe0b11e385500f87256fcd0dca2993
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-09-22 14:11:24 +0330

    first commit

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,17 +1,9 @@
 // menu
 
 let hamIcon = document.getElementById('hamIcon');
-let overlay = document.getElementById('overlay');
 
 hamIcon.addEventListener('click', function () {
   hamIcon.classList.toggle('active');
-  overlay.classList.toggle('active');
   document.querySelector('.mobileMenu').classList.toggle('active');
   document.querySelector('header').classList.toggle('active');
-});
-overlay.addEventListener('click', function () {
-  hamIcon.classList.remove('active');
-  overlay.classList.remove('active');
-  document.querySelector('.mobileMenu').classList.remove('active');
-  document.querySelector('header').classList.remove('active');
 });
